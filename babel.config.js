c25ef23b1656261a27c28ffdae7a144e5dbd7038
huyduc <huyduc2712.git@gmail.com>
2023-12-15 15:26:13 +0700

    update code

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,4 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
   plugins: ["react-native-reanimated/plugin"],
-  plugins: [
-    [
-      "module-resolver",
-      {
-        root: ["./src"],
-        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
-        alias: {
-          tests: ["./tests/"],
-          "@components": "./src/components",
-        },
-      },
-    ],
-  ],
 };
